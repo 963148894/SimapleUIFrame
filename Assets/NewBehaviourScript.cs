58c96031871f47ef5f42edd3f372e9f6631023a7
郭双建 <963148894@qq.com>
2018-08-29 14:22:01 +0800

    init

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace aa
+{
+    public class NewBehaviourScript : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            print(GetType());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
